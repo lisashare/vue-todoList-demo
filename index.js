26436fc8f617c5e0a026b575db87a6e9fcacf30e
lisashare <lisa_zhu2012@foxmail.com>
2018-06-20 15:42:03 +0800

    点击star显示inout复选框布局完成

--- index.js
+++ index.js
@@ -2,19 +2,29 @@ new Vue({
     el: "#app",
     data: {
         todos:[
-            {id:1,content:"在控制要显示的数据的时候，千万不要通过点击按钮来直接控制所有的数据，而是利用计算属性，创造出两个新的数据。一个全部都是完成的，一个全部都是未完成的，然后再创造一个真正要显示的数据",isFinished:false},
-            {id:2,content:"这是一个用纯文本的简单卡片。但卡片可以包含自己的页头，页脚，列表视图，图像，和里面的任何元素。",isFinished:true},
-            {id:3,content:"这是一个用纯文本的简单卡片。但卡片可以包含自己的页头，页脚，列表视图，图像，和里面的任何元素。",isFinished:true},
-            {id:4,content:"这是一个用纯文本的简单卡片.",isFinished:true}
+            {id:1,content:"在控制要显示的数据的时候，千万不要通过点击按钮来直接控制所有的数据，而是利用计算属性，创造出两个新的数据。一个全部都是完成的，一个全部都是未完成的，然后再创造一个真正要显示的数据",isFinished:false,isSelected:false},
+            {id:2,content:"这是一个用纯文本的简单卡片。但卡片可以包含自己的页头，页脚，列表视图，图像，和里面的任何元素。",isFinished:true,isSelected:false},
+            {id:3,content:"这是一个用纯文本的简单卡片。但卡片可以包含自己的页头，页脚，列表视图，图像，和里面的任何元素。",isFinished:true,isSelected:false},
+            {id:4,content:"这是一个用纯文本的简单卡片.",isFinished:true,isSelected:false}
         ],
         showBtns:[ //控制显示类型的按钮
             {id:1,title:'A',type:'all',theme:'success'},
             {id:2,title:'F',type:'finished',theme:'primary'},
-            {id:3,title:'U',type:'unfinished',theme:'danger'},
+            {id:3,title:'U',type:'unfinished',theme:'warning'},
         ],
         activeShowType:'all', //用来处理到底要显示什么类型的数据
         isRemoveShow:false,
         preRemoveId:null, //准备要删除的todo的id
+        isInpShow:false,
+        newtitle:'',
+        isSelectShow:true,
+        navs:[
+            {id:1,title:'A',type:'all',theme:'success'},
+            {id:2,title:'F',type:'finished',theme:'primary'},
+            {id:3,title:'U',type:'unfinished',theme:'warning'},
+            {id:4,title:'C',type:'remove',theme:'danger'}
+        ],
+        activeShowNavType:'all'
     },
     methods: {
         //点击删除按钮删除数据的方法
@@ -36,6 +46,9 @@ new Vue({
             this.todos = this.todos.filter(todo=>{
                 return todo.id !== id ? todo : false;
             })
+        },
+        add(){
+            // @click="selectMethod(nav.id)"  
         }
     },
     computed: {
@@ -48,7 +61,6 @@ new Vue({
         },
         //全部都是未完成的
         unfinishedTodos(){
-        	//根据现有的全部的todos去生成一个全部都是未完成的todos新数据
             return this.todos.filter( todo =>{
                 return !todo.isFinished ? 'todo':false;
             })
