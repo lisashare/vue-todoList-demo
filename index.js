4a4223be14bdfcfa1b5ee5408c459862e2ae8e33
lisashare <lisa_zhu2012@foxmail.com>
2018-06-21 16:53:40 +0800

    '写入内容首尾去空格，空内容提示，使用sui的toast完成一些交互'

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 // 任意的功能：1.找见关键数据 2.将数据与视图建立联系3.操控数据
-//在控制要显示的数据的时候，千万不要通过点击按钮来直接控制所有的数据，而是利用计算属性，创造出两个新的数据。一个全部都是完成的，一个全部都是未完成的，然后再创造一个真正要显示的数据
 
-//时间戳
+//时间戳：格式化时间
 Vue.filter('premiere',(value)=>{
-    //引用类型的需要深拷贝
+    //如果是引用类型的需要深拷贝
     let date = new Date(value)
     return date.getFullYear() + '年' + (date.getMonth()+1) + '月'+ date.getDate() + "日";
 })
+
 //创建vue实例
 new Vue({
-    el: "#app", //将实例挂载在#app上
+    el: "#app",     //将实例挂载在#app上
     data: {
         //下面两条数据应该从本地存储里取出使用
         count_id:JSON.parse(localStorage.todos||'[]').length,
-        todos:JSON.parse(localStorage.todos||'[]'), //存放所有todos
-        controlNav:[ //控制普通模式的操控按钮，控制显示什么类型的todo
+        todos:JSON.parse(localStorage.todos||'[]'),         //存放所有todos
+        controlNav:[            //控制普通模式的操控按钮，控制显示什么类型的todo
             {id:1,title:'A',type:'all',theme:'success'},
             {id:2,title:'F',type:'finished',theme:'primary'},
             {id:3,title:'U',type:'unfinished',theme:'warning'},
         ],
-        activeShowType:'all', //用来处理到底要显示什么类型的数据
-        isRemoveShow:false,
-        preRemoveId:null, //准备要删除的todo的id
-        isInpShow:false, //关键数据：控制输入布局的
-        newtitle:'', //新增todo的input绑定的数据
-        isSelectShow:false, //关键数据：控制按钮切换模式
-        selectNavs:[ //选择模式的操控按钮
+        activeShowType:'all',   //用来处理到底要显示什么类型的数据
+        isRemoveShow:false,     //控制删除todo弹出层是否显示
+        preRemoveId:null,       //准备要删除的todo的id
+        isInpShow:false,        //关键数据：控制输入布局的
+        newtitle:'',            //新增todo的input绑定的数据
+        isSelectShow:false,     //关键数据：控制按钮切换模式
+        selectNavs:[            //选择模式的操控按钮
             {id:1,title:'A',theme:'success'},
             {id:2,title:'F',theme:'primary'},
             {id:3,title:'U',theme:'warning'},
             {id:4,title:'C',theme:'danger'}
         ],
-        controlIndex:1 //关键数据，控制显示什么类型的todo
+        controlIndex:1          //关键数据，控制显示什么类型的todo
     },
     methods: {
         //点击删除按钮删除数据的方法
@@ -39,8 +39,8 @@ new Vue({
             
             //如果未完成做出弹出框提示
             if(!isFinished){
-                this.isRemoveShow = true;
-                this.preRemoveId = id;//把准备要删除的id先存一波
+                this.isRemoveShow = true;   //控制弹窗显示的数据
+                this.preRemoveId = id;      //把准备要删除的id先存一波
                 return false;
             }
             this.removeTodo(id);
@@ -55,8 +55,9 @@ new Vue({
             })
             //更新本地存储
             localStorage.todos = JSON.stringify(this.todos);
+            $.toast("删除成功");
         },
-        finishTodo(id){ //完成某个todo
+        finishTodo(id){   //完成某个todo
             this.todos.forEach((item=>{
                 if(item.id == id){
                     return item.isFinished = !item.isFinished;
@@ -64,18 +65,22 @@ new Vue({
             }))
             localStorage.todos = JSON.stringify(this.todos);
         },
-        add(){  //增加新的todo
-            this.todos.unshift({
-                title:this.newtitle,
-                isFinished:false,
-                id:++this.count_id,
-                time:Date.now()
-            })
-            //使新增布局消失
-            this.isInpShow = false;
-            localStorage.todos = JSON.stringify(this.todos)
+        add(){            //增加新的todo
+            if(!this.newtitle){
+                $.toast("内容不能为空");
+            }else{
+                this.todos.unshift({
+                    title:this.newtitle,
+                    isFinished:false,
+                    id:++this.count_id,
+                    time:Date.now()
+                })
+                //使新增布局消失
+                this.isInpShow = false;
+                localStorage.todos = JSON.stringify(this.todos)
+            }          
         },
-        selectAll(){ //全选
+        selectAll(){       //全选功能
             let isAll = this.todos.every((item)=>{
                 return item.isSelected;
             })
@@ -83,7 +88,7 @@ new Vue({
                 return item.isSelected = !isAll;
             })
         },
-        controlAll(type){
+        controlAll(type){   //根据点击哪个按钮，改变选中的项目是否完成
             this.todos.forEach((item)=>{
                 if(item.isSelected){
                     item.isFinished = type;
@@ -91,7 +96,7 @@ new Vue({
             })
             localStorage.todos = JSON.stringify(this.todos)
         },
-        removeAll(){ //删除所有
+        removeAll(){        //删除所有选中的todos
             this.todos=this.todos.filter((item)=>{
                 if(!item.isSelected){
                     return item;  //判断如果未选中,返回未选中项
